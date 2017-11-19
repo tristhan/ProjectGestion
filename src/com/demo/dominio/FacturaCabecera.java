e6f44c773173154475ec03031d7a9ada99f641bf
tristhan <tristhan1993@hotmail.com>
2017-11-18 21:03:32 -0500

    1
    as

---
+++ src/com/demo/dominio/FacturaCabecera.java
@@ -0,0 +1,131 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.demo.dominio;
+
+import java.sql.Date;
+import java.util.List;
+
+/**
+ * @author Denisse Bailon
+ */
+public class FacturaCabecera {
+    private int id_facturaCab;
+    private List <Cliente> cliente;
+    private List <Empleado> empleado;
+    private Date fecha;
+    private String Telefono;
+    private String direccion;
+    private double descuento;
+    private double subtotal;
+    private double iva;
+    private double totalPagar;
+
+    public FacturaCabecera() {
+    }
+    
+
+    public FacturaCabecera(int id_facturaCab, List<Cliente> cliente, List<Empleado> empleado, Date fecha, String Telefono, String direccion, double descuento, double subtotal, double iva, double totalPagar) {
+        this.id_facturaCab = id_facturaCab;
+        this.cliente = cliente;
+        this.empleado = empleado;
+        this.fecha = fecha;
+        this.Telefono = Telefono;
+        this.direccion = direccion;
+        this.descuento = descuento;
+        this.subtotal = subtotal;
+        this.iva = iva;
+        this.totalPagar = totalPagar;
+    }
+
+    public int getId_facturaCab() {
+        return id_facturaCab;
+    }
+
+    public void setId_facturaCab(int id_facturaCab) {
+        this.id_facturaCab = id_facturaCab;
+    }
+
+    public List<Cliente> getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(List<Cliente> cliente) {
+        this.cliente = cliente;
+    }
+
+    public List<Empleado> getEmpleado() {
+        return empleado;
+    }
+
+    public void setEmpleado(List<Empleado> empleado) {
+        this.empleado = empleado;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getTelefono() {
+        return Telefono;
+    }
+
+    public void setTelefono(String Telefono) {
+        this.Telefono = Telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public double getDescuento() {
+        return descuento;
+    }
+
+    public void setDescuento(double descuento) {
+        this.descuento = descuento;
+    }
+
+    public double getSubtotal() {
+        return subtotal;
+    }
+
+    public void setSubtotal(double subtotal) {
+        this.subtotal = subtotal;
+    }
+
+    public double getIva() {
+        return iva;
+    }
+
+    public void setIva(double iva) {
+        this.iva = iva;
+    }
+
+    public double getTotalPagar() {
+        return totalPagar;
+    }
+
+    public void setTotalPagar(double totalPagar) {
+        this.totalPagar = totalPagar;
+    }
+
+    @Override
+    public String toString() {
+        return "FacturaCabecera{" + "id_facturaCab=" + id_facturaCab + ", cliente=" + cliente + ", empleado=" + empleado + ", fecha=" + fecha + ", Telefono=" + Telefono + ", direccion=" + direccion + ", descuento=" + descuento + ", subtotal=" + subtotal + ", iva=" + iva + ", totalPagar=" + totalPagar + '}';
+    }
+    
+
+    
+            
+}
---
+++ src/com/demo/dominio/FacturaDetalle.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.demo.dominio;
+
+import java.util.List;
+
+/**
+ *
+ * @author Denisse Bailon
+ */
+public class FacturaDetalle {
+    private int id_detalleFactura;
+    private List <FacturaCabecera> facturaCabezera;
+    private List <Articulo> articulo;
+    private int cantidad;
+    private double precioUnitario;
+
+    public FacturaDetalle() {
+    }
+    
+
+    public FacturaDetalle(int id_detalleFactura, List<FacturaCabecera> facturaCabezera, List<Articulo> articulo, int cantidad, double precioUnitario) {
+        this.id_detalleFactura = id_detalleFactura;
+        this.facturaCabezera = facturaCabezera;
+        this.articulo = articulo;
+        this.cantidad = cantidad;
+        this.precioUnitario = precioUnitario;
+    }
+
+    public int getId_detalleFactura() {
+        return id_detalleFactura;
+    }
+
+    public void setId_detalleFactura(int id_detalleFactura) {
+        this.id_detalleFactura = id_detalleFactura;
+    }
+
+    public List<FacturaCabecera> getFacturaCabezera() {
+        return facturaCabezera;
+    }
+
+    public void setFacturaCabezera(List<FacturaCabecera> facturaCabezera) {
+        this.facturaCabezera = facturaCabezera;
+    }
+
+    public List<Articulo> getArticulo() {
+        return articulo;
+    }
+
+    public void setArticulo(List<Articulo> articulo) {
+        this.articulo = articulo;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public double getPrecioUnitario() {
+        return precioUnitario;
+    }
+
+    public void setPrecioUnitario(double precioUnitario) {
+        this.precioUnitario = precioUnitario;
+    }
+
+    @Override
+    public String toString() {
+        return "FacturaDetalle{" + "id_detalleFactura=" + id_detalleFactura + ", facturaCabezera=" + facturaCabezera + ", articulo=" + articulo + ", cantidad=" + cantidad + ", precioUnitario=" + precioUnitario + '}';
+    }
+    
+    
+    
+}
